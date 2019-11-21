bce267becf7315c2b8543fc06e71659d871dc4c1
ugeenalex <ugeenalex@gmail.com>
2019-11-21 18:33:44 +0600

    Add basic interface code

--- catalog.py
+++ catalog.py
@@ -1,11 +1,8 @@
 import os
 import wx
 
+# vars
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Directory Creator')
-frame.Show()
-app.MainLoop()
 
 path = os.getcwd()
 mainDirName = input('Enter main folder name: ')
@@ -13,6 +10,40 @@ subDirName = ''
 List = open('sub.txt').read().splitlines()
 subDirNames = List
 listLength = len(subDirNames)
+
+
+# interface
+
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Hello World')
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.text_ctrl = wx.TextCtrl(panel)
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        my_btn = wx.Button(panel, label='Press Me')
+        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
+
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+        if not value:
+            print("You didn't enter anything!")
+        else:
+            print(f'You typed: "{value}"')
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+
+
+# logic
+
 
 # create main dir
 try:
