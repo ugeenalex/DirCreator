c2ca561047f53ad288951283f4f5eadc14b8ca30
ugeenalex <ugeenalex@gmail.com>
2019-11-22 10:54:42 +0600

    Add basic interface code

--- catalog.py
+++ catalog.py
@@ -6,6 +6,7 @@ import wx
 
 path = os.getcwd()
 mainDirName = input('Enter main folder name: ')
+subDirPrefix = input('Enter subdirectory prefix: ')
 subDirName = ''
 List = open('sub.txt').read().splitlines()
 subDirNames = List
@@ -15,31 +16,31 @@ listLength = len(subDirNames)
 # interface
 
 
-class MyFrame(wx.Frame):
-    def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)
-        my_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
-        my_btn = wx.Button(panel, label='Press Me')
-        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
-        panel.SetSizer(my_sizer)
-        self.Show()
-
-    def on_press(self, event):
-        value = self.text_ctrl.GetValue()
-        if not value:
-            print("You didn't enter anything!")
-        else:
-            print(f'You typed: "{value}"')
-
-
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+# class MyFrame(wx.Frame):
+#     def __init__(self):
+#         super().__init__(parent=None, title='Hello World')
+#         panel = wx.Panel(self)
+#         my_sizer = wx.BoxSizer(wx.VERTICAL)
+#         self.text_ctrl = wx.TextCtrl(panel)
+#         my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+#         my_btn = wx.Button(panel, label='Press Me')
+#         my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+#         my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+#         panel.SetSizer(my_sizer)
+#         self.Show()
+#
+#     def on_press(self, event):
+#         value = self.text_ctrl.GetValue()
+#         if not value:
+#             print("You didn't enter anything!")
+#         else:
+#             print(f'You typed: "{value}"')
+#
+#
+# if __name__ == '__main__':
+#     app = wx.App()
+#     frame = MyFrame()
+#     app.MainLoop()
 
 
 # logic
@@ -55,7 +56,7 @@ except FileExistsError:
 # create subdir
 for i in range(listLength):
     try:
-        os.makedirs(os.path.join(mainDirName, subDirNames[i], 'html_' + subDirNames[i]))
+        os.makedirs(os.path.join(mainDirName, subDirNames[i], subDirPrefix + '_' + subDirNames[i]))
         print('Folder created: ' + subDirNames[i])
     except FileExistsError:
         print('Folder exist: ' + subDirNames[i])
